b2e73780dee4b5ce8d25a54133d8c6765f4e51b5
Keziah Heynes <kheynes@c3r2s8.wethinkcode.co.za>
2019-08-09 13:23:26 +0200

    Not meeting while loop condition for test

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,12 @@
 /*   By: kheynes <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 12:05:40 by kheynes           #+#    #+#             */
-/*   Updated: 2019/08/06 16:17:10 by kheynes          ###   ########.fr       */
+/*   Updated: 2019/08/09 13:20:42 by kheynes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 static char	*increasebuf(const int fd, char *buf, int *r)
 {
@@ -39,10 +40,10 @@ int			get_next_line(const int fd, char **line)
 
 	if (!line || fd < 0)
 		return (-1);
+	r = 1;
 	if (!buf)
 		buf = ft_strnew(0);
-	r = 1;
-	while (r < 0)
+	while (r > 0)
 	{
 		if ((s = ft_strchr(buf, '\n')) != NULL)
 		{
@@ -57,3 +58,40 @@ int			get_next_line(const int fd, char **line)
 		*line = ft_strnew(0);
 	return (r);
 }
+int				main(void)
+{
+	char		*line;
+	int			fd;
+	int			ret;
+	int			count_lines;
+	char		*filename;
+	int			errors;
+
+	filename = "gnl7_1.txt";
+	fd = open(filename, O_RDONLY);
+	if (fd > 2)
+	{
+		count_lines = 0;
+		errors = 0;
+		line = NULL;
+		while ((ret = get_next_line(fd, &line)) > 0)
+		{
+			if (count_lines == 0 && strcmp(line, "12345678") != 0)
+				errors++;
+			count_lines++;
+			if (count_lines > 50)
+				break ;
+		}
+		close(fd);
+		if (count_lines != 1)
+			printf("-> must have returned '1' once instead of %d time(s)\n", count_lines);
+		if (errors > 0)
+			printf("-> must have read \"12345678\" instead of \"%s\"\n", line);
+		if (count_lines == 1 && errors == 0)
+			printf("OK\n");
+	}
+	else
+		printf("An error occured while opening file %s\n", filename);
+	return (0);
+}
+
